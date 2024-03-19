80735ad1d9b48fc094cb06b94f075f5a564e3ffd
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-03-19 15:42:18 -0300

    Implementação da lib blessed.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,64 @@
+import blessed from 'blessed';
+
 function main() {
-    console.log("Hello World.");
+    // Create a screen object.
+    var screen = blessed.screen({
+        smartCSR: true
+    });
+
+    screen.title = 'PatroTerminal';
+
+    // Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+    top: 'center',
+    left: 'center',
+    width: '50%',
+    height: '50%',
+    content: 'Hello {bold}world{/bold}!',
+    tags: true,
+    border: {
+      type: 'line'
+    },
+    style: {
+      fg: 'white',
+      bg: 'magenta',
+      border: {
+        fg: '#f0f0f0'
+      },
+      hover: {
+        bg: 'green'
+      }
+    }
+  });
+  
+  // Append our box to the screen.
+  screen.append(box);
+  
+  // If our box is clicked, change the content.
+  box.on('click', function(_data: any) {
+    box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+    screen.render();
+  });
+  
+  // If box is focused, handle `enter`/`return` and give us some more content.
+  box.key('enter', function(_ch: any, _key: any) {
+    box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
+    box.setLine(1, 'bar');
+    box.insertLine(1, 'foo');
+    screen.render();
+  });
+  
+  // Quit on Escape, q, or Control-C.
+  screen.key(['escape', 'q', 'C-c'], function(_ch: any, _key: any) {
+    return process.exit(0);
+  });
+  
+  // Focus our element.
+  box.focus();
+  
+  // Render the screen.
+  screen.render();
+
 }
 
-main();
+main(); 
